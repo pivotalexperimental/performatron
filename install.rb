9c7c4c66e7905b058717b9e20d3adeaf78d3c5da
David Stevenson <david@flouri.sh>
2009-02-28 10:43:40 -0800

    Install.rb not using full paths

--- install.rb
+++ install.rb
@@ -4,7 +4,8 @@ default_dir = File.dirname(__FILE__) + "/default"
 unless File.exist?("#{RAILS_ROOT}/config/performatron.yml")
   FileUtils.cp(default_dir + "/performatron.yml", "#{RAILS_ROOT}/config/performatron.yml")
 end
-Dir[default_dir + "/performatron/*"].each do |filename|
+Dir[default_dir + "/performatron/*"].each do |filepath|
+  filename = File.basename(filepath)
   new_path = "#{RAILS_ROOT}/lib/performatron/#{filename}"
   FileUtils.cp(default_dir + "/performatron/" + filename, new_path) unless File.exist?(new_path)
 end
