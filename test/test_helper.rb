84b25565eaa35c65ef948ce170683ef34451fe8d
Chad Woolley <thewoolleyman@gmail.com>
2009-03-21 11:47:43 -0700

    support specification of DB port for ci

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,12 +6,16 @@ require 'active_support/test_case'
 require "active_record"
 require 'test/db_helper'
 
-ActiveRecord::Base.configurations = {'test' => {
-             'adapter'  => 'mysql',
-             'database' => 'performatron_test',
-              'username' => ENV['DB_USER'],
-              'password' => ENV['DB_PASSWORD'] 
-          }}
+db_config = {
+  'adapter'  => 'mysql',
+  'database' => 'performatron_test',
+  'username' => ENV['DB_USER'],
+  'password' => ENV['DB_PASSWORD']
+}
+
+db_config['socket'] = ENV['DB_SOCKET'] if ENV['DB_SOCKET']
+
+ActiveRecord::Base.configurations = {'test' => db_config}
 
 RAILS_ENV="test"
 ENV["SCHEMA"] = File.dirname(__FILE__) + "/schema.rb"
@@ -20,7 +24,8 @@ ActiveRecord::Base.establish_connection :test
 
 begin
   load ENV["SCHEMA"]
-rescue 
+rescue Mysql::Error => e
+  p e
   p '----------------------------------------------------------------------------------------------------------------'
   p "You need to create the test database by running 'rake test:build_database' [DB_USER=user] [DB_PASSWORD=password]"
   p '----------------------------------------------------------------------------------------------------------------'
